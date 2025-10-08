e3ae3c15f0b880965b254e48afafd1c25c010e02
KienPC <trinhtrungkien165@gmail.com>
2025-10-08 16:56:32 +0700

    Uppdate file PatientSearchServlet.java
    Import thư viện import jakarta.servlet.http.*;

--- src/java/control/PatientSearchServlet.java
+++ src/java/control/PatientSearchServlet.java
@@ -12,7 +12,9 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
+import java.io.IOException;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
 /**
  *
  * @author Admin
