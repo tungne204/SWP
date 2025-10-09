695e93082d437e652b5499a8f67d41524f416a09
KienPC <trinhtrungkien165@gmail.com>
2025-10-09 17:37:14 +0700

    Update class PatientSearchServlet.java in Package control
    bỏ comment if-else, thêm try catch: List<Patient> list;
    
            try {
                if (keyword != null && !keyword.trim().isEmpty()) {
                    list = dao.searchPatients(keyword);
                } else {
                    list = dao.getAllPatients();
                }
    
                request.setAttribute("patients", list);
                request.setAttribute("keyword", keyword);
                request.getRequestDispatcher("patientSearch.jsp").forward(request, response);
    
            } catch (Exception e) {
                e.printStackTrace();
                request.setAttribute("error", "Lỗi khi tải dữ liệu bệnh nhân!");
                request.getRequestDispatcher("error.jsp").forward(request, response);
            }

--- src/java/control/PatientSearchServlet.java
+++ src/java/control/PatientSearchServlet.java
@@ -8,6 +8,14 @@ import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.*;
 
+/**
+ * Servlet handles View/Search Patient 
+ * function For Receptionist module in SWP
+ * system - Displays the entire patient list -
+ * Search by name, ID, or address
+ *
+ * URL: /PatientSearchServlet
+ */
 @WebServlet("/PatientSearchServlet")
 public class PatientSearchServlet extends HttpServlet {
 
@@ -15,18 +23,28 @@ public class PatientSearchServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
+        response.setCharacterEncoding("UTF-8");
+        request.setCharacterEncoding("UTF-8");
+
         String keyword = request.getParameter("keyword");
         PatientDAO dao = new PatientDAO();
         List<Patient> list;
 
-//        if (keyword != null && !keyword.trim().isEmpty()) {
-//            list = dao.searchPatients(keyword);
-//        } else {
-//            list = dao.getAllPatients();
-//        }
-//
-//        request.setAttribute("patients", list);
-//        request.setAttribute("keyword", keyword);
-//        request.getRequestDispatcher("patientSearch.jsp").forward(request, response);
+        try {
+            if (keyword != null && !keyword.trim().isEmpty()) {
+                list = dao.searchPatients(keyword);
+            } else {
+                list = dao.getAllPatients();
+            }
+
+            request.setAttribute("patients", list);
+            request.setAttribute("keyword", keyword);
+            request.getRequestDispatcher("patientSearch.jsp").forward(request, response);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            request.setAttribute("error", "Lỗi khi tải dữ liệu bệnh nhân!");
+            request.getRequestDispatcher("error.jsp").forward(request, response);
+        }
     }
 }
