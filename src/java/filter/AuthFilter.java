2966bfdac5c6274550ea533978e3fa6314fb5ea2
Do Quang Anh <quanganhsk1720@gmail.com>
2025-10-19 23:30:44 +0700

    them authen cho chuc nang cua doctor va manager

--- src/java/filter/AuthFilter.java
+++ src/java/filter/AuthFilter.java
@@ -6,7 +6,7 @@ import jakarta.servlet.http.*;
 import java.io.IOException;
 import entity.User;
 
-@WebFilter(urlPatterns = {"/doctor/*", "/receptionist/*", "/manager/*", "/patient-queue/*", "/medical-assistant/*"})
+@WebFilter(urlPatterns = {"/doctor/*", "/receptionist/*", "/manager/*", "/patient-queue/*", "/medical-assistant/*","/set-permission","/appointment","/test-result","/medical-report"})
 public class AuthFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
@@ -26,7 +26,7 @@ public class AuthFilter implements Filter {
         int roleId = acc.getRoleId();
         String uri = req.getRequestURI();
 
-        if (uri.contains("/doctor/") && roleId != 2) {
+        if ((uri.contains("/doctor/") || uri.contains("/appointment")|| uri.contains("/test-result") || uri.contains("/medical-report")) && roleId != 2) {
             res.sendRedirect(req.getContextPath() + "/403.jsp");
             return;
         }
@@ -34,7 +34,7 @@ public class AuthFilter implements Filter {
             res.sendRedirect(req.getContextPath() + "/403.jsp");
             return;
         }
-        if (uri.contains("/manager/") && roleId != 1) {
+        if ((uri.contains("/manager/") || uri.contains("/set-permission")) && roleId != 1) {
             res.sendRedirect(req.getContextPath() + "/403.jsp");
             return;
         }
